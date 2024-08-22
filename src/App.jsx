79118c7a407dc341316eeb70b6d495656c2c24eb
Emilse Arias <emilsearias@gmail.com>
2024-08-22 16:40:41 -0300

    agregamos un ejemplo de maquetado en el componente App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   
   return (
-    <h1>hola mundo</h1>
+    <main className='container my-4'>
+      <h1 className='text-center'>Primer proyecto react</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ullam repellat vel reiciendis? Porro, architecto <br/> voluptatem fuga<br></br> quae ad, saepe perferendis eius ut sint animi, obcaecati libero fugit natus molestiae.</p>
+    </main>
   )
 }
 
