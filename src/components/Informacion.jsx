056734d1423839fe90159d309e693edecf2e4c9c
Emilse Arias <emilsearias@gmail.com>
2024-08-27 16:16:44 -0300

    enviamos un props de prueba

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -9,7 +9,8 @@ const Informacion = () => {
             {/* aqui agrego comentario del maquetado */}
             <h2>Introducción a react</h2>
             <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. Se destaca por su enfoque en la creación de aplicaciones web de una sola <strong>página SPA</strong> y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un <span className="parrafoResaltado">DOM virtual</span>. React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
-            <Teoria></Teoria>
+            {/* enviar un props */}
+            <Teoria comision='101i' ></Teoria>
         </section>
     );
 };
