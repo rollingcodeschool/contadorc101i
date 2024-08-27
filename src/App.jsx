ef99f0afeaa93f910d10d2777682b8da832643ee
Emilse Arias <emilsearias@gmail.com>
2024-08-27 15:46:19 -0300

    agregamos el componente Información con texto sobre react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/Informacion";
 
 function App() {
-  
   return (
-    <main className='container my-4'>
-      <h1 className='text-center'>Primer proyecto react</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ullam repellat vel reiciendis? Porro, architecto <br/> voluptatem fuga<br></br> quae ad, saepe perferendis eius ut sint animi, obcaecati libero fugit natus molestiae.</p>
+    <main className="container my-4">
+      <h1 className="text-center">Primer proyecto react</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        ullam repellat vel reiciendis? Porro, architecto <br /> voluptatem fuga
+        <br></br> quae ad, saepe perferendis eius ut sint animi, obcaecati
+        libero fugit natus molestiae.
+      </p>
+      {/* invocar los componentes */}
+      <Informacion></Informacion>
+      {/* <Informacion/> */}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,14 @@
+const Informacion = () => {
+// aqui agrego la mayor parte de logica
+
+    return (
+        // algo de logica
+        <section>
+            {/* aqui agrego comentario del maquetado */}
+            <h2>Introducción a react</h2>
+            <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. Se destaca por su enfoque en la creación de aplicaciones web de una sola <strong>página SPA</strong> y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un <span className="parrafoResaltado">DOM virtual</span>. React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
+        </section>
+    );
+};
+
+export default Informacion;
