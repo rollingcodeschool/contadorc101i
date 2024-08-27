a3bd35049f21d54ebc0c9e3be6ace2c269d3bc32
Emilse Arias <emilsearias@gmail.com>
2024-08-27 16:37:03 -0300

    modificamos el props de teoria con, desestructurando el props

--- src/components/Teoria.jsx
+++ src/components/Teoria.jsx
@@ -1,11 +1,11 @@
-const Teoria = (props) => {
-    console.log(props.anioActual)
+const Teoria = ({comision, anioActual}) => {
+    console.log(anioActual)
     return (
         <article>
             <h3>Componentes</h3>
             <p>Uno de los conceptos clave en React son los componentes. Un componente es una pieza reutilizable de la interfaz de usuario que se encarga de representar una parte específica de la pantalla. Los componentes pueden ser tan simples como un botón o tan complejos como un formulario completo. React permite la creación de componentes tanto de clase como funcionales, aunque los componentes funcionales se han vuelto más populares con la introducción de los Hooks.</p>
             <p>Cada componente en React puede tener su propio estado (state) y propiedades (props). El estado es una forma de almacenar y manejar datos internos del componente, mientras que las propiedades son datos que se pasan de un componente padre a un componente hijo. Al combinar estos elementos, React permite la creación de interfaces interactivas, donde los cambios en los datos se reflejan automáticamente en la interfaz de usuario.</p>
-            <h4>Comision: {props.comision} - Año actual: {props.anioActual}</h4>
+            <h4>Comision: {comision} - Año actual:  {anioActual}</h4>
         </article>
     );
 };
