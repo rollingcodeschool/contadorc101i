82df27af2ec87b4c71b804e5e6696e9cfa4479ae
Emilse Arias <emilsearias@gmail.com>
2024-08-29 14:52:00 -0300

    agregamos el maquetado del componente contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Informacion from "./components/Informacion";
+import Contador from "./components/Contador";
 
 function App() {
   return (
-    <main className="container my-4">
-      <h1 className="text-center">Primer proyecto react</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-        ullam repellat vel reiciendis? Porro, architecto <br /> voluptatem fuga
-        <br></br> quae ad, saepe perferendis eius ut sint animi, obcaecati
-        libero fugit natus molestiae.
-      </p>
-      {/* invocar los componentes */}
-      <Informacion></Informacion>
-      {/* <Informacion/> */}
-    </main>
+    // <>
+      <main className="container my-4">
+        <h1 className="text-center">Primer proyecto react</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          ullam repellat vel reiciendis? Porro, architecto <br /> voluptatem
+          fuga
+          <br></br> quae ad, saepe perferendis eius ut sint animi, obcaecati
+          libero fugit natus molestiae.
+        </p>
+        {/* invocar los componentes */}
+        <Informacion></Informacion>
+        {/* <Informacion/> */}
+        <Contador></Contador>
+      </main>
+    // </>
   );
 }
 
---
+++ src/components/Contador.jsx
@@ -0,0 +1,12 @@
+const Contador = () => {
+    return (
+        <section className="text-center my-5">
+            <h2>Contador</h2>
+            <h4>10</h4>
+            <button className="btn btn-primary me-2">+1</button>
+            <button className="btn btn-warning">-1</button>
+        </section>
+    );
+};
+
+export default Contador;
