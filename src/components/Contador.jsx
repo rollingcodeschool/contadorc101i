dcd84171b722e1c7ec56bf14b1125490cb49c6b8
Emilse Arias <emilsearias@gmail.com>
2024-08-29 15:17:05 -0300

    creamos el estado numero

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 const Contador = () => {
+//logica del componente
+// let numero= 1;
+//crear un estado
+const [numero, setNumero] = useState(1);
+
+const sumar = ()=>{
+    numero++;
+    console.log(numero)
+}
+
     return (
         <section className="text-center my-5">
             <h2>Contador</h2>
-            <h4>10</h4>
-            <button className="btn btn-primary me-2">+1</button>
+            <h4>{numero}</h4>
+            <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
             <button className="btn btn-warning">-1</button>
         </section>
     );
